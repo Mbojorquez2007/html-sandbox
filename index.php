f1f64969fa4f62bdb6322ae04f117bcc419b5552
Michelle <mbojorquez4@cnm.edu>
2017-07-12 14:00:19 -0600

    added table

--- index.php
+++ index.php
@@ -5,5 +5,26 @@
 	</head>
 	<body>
 		<h1>This is my web page</h1>
+
+		<table>
+			<tr><th>Cats</th>
+			<th>Dogs</th></tr>
+			<tr></tr>
+			<tr>
+			<td>Persian</td>
+			<td>Beagle</td>
+			</tr>
+			<tr>
+			<td>Maine Coon</td>
+			<td>Rottneiler</td>
+			</tr>
+			<tr>
+			<td>Siamese</td>
+			<td>German Shepard</td>
+			</tr>
+		</table>
+
+		<-- This is an HTML comment -->
+		<h2>A few of My Favorite Things</h2>
 	</body>
 </html>
